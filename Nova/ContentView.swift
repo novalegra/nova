287f10b710196f1e8af450b891a12780402d7364
Anna Quinlan <anna.quinlan123@gmail.com>
2020-09-07 18:22:47 -0700

    Initial Commit

---
+++ Nova/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nova
+//
+//  Created by Anna Quinlan on 9/7/20.
+//  Copyright © 2020 Anna Quinlan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
