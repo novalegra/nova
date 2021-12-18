da4da2382f381afa9f543f114fb00bc8c92e0f78
AQ <anna.quinlan123@gmail.com>
2021-12-18 14:55:54 -0800

    Add test files & bump to iOS 15 / WatchOS 8

---
+++ NovaTests/MenstrualDataManagerTests.swift
@@ -0,0 +1,9 @@
+//
+//  MenstrualDataManagerTests.swift
+//  NovaTests
+//
+//  Created by Anna Quinlan on 12/18/21.
+//  Copyright © 2021 Anna Quinlan. All rights reserved.
+//
+
+import Foundation
