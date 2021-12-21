e5cc1c6086ea7c036015d60ac9ef4cfd399d6654
AQ <anna.quinlan123@gmail.com>
2021-12-20 17:58:31 -0800

    Add tests for saving/updating logic

--- NovaTests/MenstrualStoreTests.swift
+++ NovaTests/MenstrualStoreTests.swift
@@ -7,18 +7,20 @@
 //
 
 import XCTest
+import HealthKit
 @testable import Nova
 
 class MenstrualStoreTests: XCTestCase {
     let testDate = Date(timeIntervalSince1970: TimeInterval(30))
     
     var menstrualStore: MenstrualStore!
+    var healthStore: HKHealthStoreMock!
     var mockSample1: MenstrualSample!
     var mockSample2: MenstrualSample!
 
     override func setUpWithError() throws {
         super.setUp()
-        let healthStore = HKHealthStoreMock()
+        healthStore = HKHealthStoreMock()
 
         menstrualStore = MenstrualStore(healthStore: healthStore)
         mockSample1 = MenstrualSample(startDate: testDate, endDate: testDate, flowLevel: .light)
@@ -101,6 +103,125 @@ class MenstrualStoreTests: XCTestCase {
         XCTAssertEqual(menstrualStore.flowLevel(for: .hadFlow, with: -1), .unspecified)
     }
     
+    // MARK: getAction
+    func testActionShouldSave() {
+        let result = menstrualStore.action(
+            existingSample: nil,
+            date: Date(),
+            newVolume: 5,
+            flowSelection: .hadFlow)
+        
+        XCTAssertEqual(result, .saveNewSample)
+    }
+    
+    func testActionShouldSaveNoFlow() {
+        let result = menstrualStore.action(
+            existingSample: nil,
+            date: Date(),
+            newVolume: 5,
+            flowSelection: .noFlow)
+        
+        XCTAssertEqual(result, .saveNewSample)
+    }
+    
+    func testActionShouldSaveNothingSelected() {
+        let result = menstrualStore.action(
+            existingSample: nil,
+            date: Date(),
+            newVolume: 5,
+            flowSelection: .none)
+        
+        XCTAssertEqual(result, .unknown)
+    }
+    
+    func testActionShouldUpdate() {
+        let result = menstrualStore.action(
+            existingSample: mockSample1,
+            date: mockSample1.startDate,
+            newVolume: 5,
+            flowSelection: .hadFlow)
+        
+        XCTAssertEqual(result, .updateSample)
+    }
+    
+    func testActionShouldUpdateNoFlow() {
+        let result = menstrualStore.action(
+            existingSample: mockSample1,
+            date: mockSample1.startDate,
+            newVolume: 0,
+            flowSelection: .noFlow)
+        
+        XCTAssertEqual(result, .updateSample)
+    }
+    
+    func testActionShouldDelete() {
+        let result = menstrualStore.action(
+            existingSample: mockSample1,
+            date: mockSample1.startDate,
+            newVolume: 5,
+            flowSelection: .none)
+        
+        XCTAssertEqual(result, .deleteSample)
+    }
+    
     // MARK: saveInHealthKit
-    // ANNA TODO
+    func testSaveInHealthKit() async {
+        let saveHealthStoreHandler = expectation(description: "Add health store handler")
+        
+        healthStore.setSaveHandler({[unowned self] (objects, success, error) in
+            XCTAssertEqual(1, objects.count)
+
+            let sample = objects.first as! HKCategorySample
+
+            XCTAssertEqual(sample.startDate, mockSample1.startDate)
+
+            saveHealthStoreHandler.fulfill()
+        })
+        
+        _ = await menstrualStore.saveSample(mockSample1)
+        
+        wait(for: [saveHealthStoreHandler], timeout: 10, enforceOrder: true)
+    }
+    
+    func testDeleteInHealthKit() async {
+        let deleteHealthStoreHandler = expectation(description: "Delete health store handler")
+        
+        healthStore.setDeletedObjectsHandler({[unowned self] (objectType, predicate, success, count, error) in
+            XCTAssertEqual(objectType, HKObjectType.categoryType(forIdentifier: .menstrualFlow))
+            XCTAssertEqual(predicate.predicateFormat, "UUID == \(mockSample1.uuid)")
+
+            deleteHealthStoreHandler.fulfill()
+        })
+        
+        _ = await menstrualStore.deleteSample(mockSample1)
+        
+        wait(for: [deleteHealthStoreHandler], timeout: 10, enforceOrder: true)
+    }
+    
+    func testUpdateInHealthKit() async {
+        let deleteHealthStoreHandler = expectation(description: "Delete health store handler")
+        let saveHealthStoreHandler = expectation(description: "Save health store handler")
+        
+        healthStore.setDeletedObjectsHandler({[unowned self] (objectType, predicate, success, count, error) in
+            XCTAssertEqual(objectType, HKObjectType.categoryType(forIdentifier: .menstrualFlow))
+            XCTAssertEqual(predicate.predicateFormat, "UUID == \(mockSample1.uuid)")
+
+            deleteHealthStoreHandler.fulfill()
+        })
+        
+        healthStore.setSaveHandler({[unowned self] (objects, success, error) in
+            XCTAssertEqual(1, objects.count)
+
+            let sample = objects.first as! HKCategorySample
+
+            XCTAssertEqual(sample.startDate, mockSample1.startDate)
+            XCTAssertEqual(sample.version, 2)
+
+            saveHealthStoreHandler.fulfill()
+        })
+        
+        _ = await menstrualStore.updateSample(mockSample1)
+        
+        wait(for: [deleteHealthStoreHandler, saveHealthStoreHandler], timeout: 10, enforceOrder: true)
+    }
 }
